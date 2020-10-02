0865735c4b47849e4e567c6f60ed5c819916652d
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-10-02 08:32:10 +0700

    Add files via upload

---
+++ precache-manifest.cd6e38d2454a1a9c7efbeca0702ec16e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c5d73750a54446e4786a45ac94b5b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea2790afeb6a254c87a1",
+    "url": "/static/css/main.0ae50b0e.chunk.css"
+  },
+  {
+    "revision": "33d755aa9afea30a11ca",
+    "url": "/static/js/2.50d48f6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50d48f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2790afeb6a254c87a1",
+    "url": "/static/js/main.888928c5.chunk.js"
+  },
+  {
+    "revision": "94add0f1748447182057",
+    "url": "/static/js/runtime-main.d7ab499f.js"
+  }
+]);
